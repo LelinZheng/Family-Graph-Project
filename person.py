dcecb5bf209148369939a1695ea9193e0a5da634
LelinZheng <lezi.lelin@gmail.com>
2024-11-21 15:37:49 -0800

    Update str output of person and added is_alive attribute

--- person.py
+++ person.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 
 class Person:
-    def __init__(self, name, birthdate, gender, occupation="Unknown"):
+    def __init__(self, name, birthdate, gender, occupation="Unknown", is_alive=True):
         self.name = name
-        self.birthdate = birthdate #DD-MM-YYYY
+        self.birthdate = birthdate  # DD-MM-YYYY
         self.gender = gender
         self.occupation = occupation
-        self.relation_dict = {} # key is the relation like "mother", values is a list of person object
+        self.relation_dict = {}  # key is the relation like "mother", values  a list of person object
+        self.is_alive = is_alive
     
     @property
     def age(self):
@@ -18,6 +19,9 @@ class Person:
     def update_gender(self, new_gender):
         self.gender = new_gender
     
+    def update_is_alive(self, new_is_alive):  # new_is_alive must be True or False
+        self.is_alive = new_is_alive
+
     def update_occupation(self, new_occupation):
         self.occupation = new_occupation
     
@@ -40,8 +44,16 @@ class Person:
         for (relation, people) in self.relation_dict.items():
             string += relation + ": "
             for person in people:
-                string += person.name  # + " "
+                string += person.name  + " "
             string += " ; "
-        
-        return "Name: " + self.name + " , Age: " + str(self.age) + " , Relation: " + string
+        birthday = self.birthdate.split("-")[0]
+        birthmonth = int(self.birthdate.split("-")[1])
+        month_list = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 
+                      5: "May", 6: "June", 7: "July", 8: "Aug",
+                      9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+        month = month_list[birthmonth]
+        return "Name: " + self.name + ", Gender: " + self.gender + ", Birthday: " +\
+            month + " " + birthday + " , Age: " + str(self.age) + ", Occupation: " +\
+            self.occupation + ", Is Alive: " + str(self.is_alive) + " , Immediate Family Relations: " +\
+            string
 
