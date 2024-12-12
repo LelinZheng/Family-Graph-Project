65413d7ace4065a9e4316c745c7f23c2ba11c21f
LelinZheng <lezi.lelin@gmail.com>
2024-12-11 18:04:47 -0800

    update zeynab demo file

--- demo_zeynab_family.py
+++ demo_zeynab_family.py
@@ -2,12 +2,15 @@ from person import Person
 from collections import deque
 import json
 
-# This demo file is linked to the Zeynab_family_tree.json file
+"""
+This demo file is created to test Person class and BFS searching function
+It is linked to the Zeynab_family_tree.json file
+"""
+
 
 def build_zeynab_family():
     family_dict = {}
 
-    # Zeynab could change the people in the demo family, starting from this line
     me = Person("Zeynab", "09-11-1999", "F", "Student", True)
     mom = Person("Sigge", "23-10-1962", "F", "Nurse", True)
     aunt = Person("Simagne", "09-11-1958", "F", "Jeweler", True)
@@ -15,7 +18,6 @@ def build_zeynab_family():
     grandpa = Person("Jedd", "06-08-1939", "M", "Unknown", True)
     sister = Person("Zekeya", "01-04-2002", "F", "Student", True)
     
-
     me.add_relation("mother", mom)
     me.add_relation("sibling", sister)
 
@@ -53,8 +55,8 @@ def build_zeynab_family():
 
     # Save to JSON
     save_family_to_json(family_dict)
-    # Down to this line
-    loaded_family_dict = load_family_from_json()  # Load from JSON
+    # Load from JSON
+    loaded_family_dict = load_family_from_json()
 
     # Verify loaded data
     for name, person in loaded_family_dict.items():
@@ -78,8 +80,8 @@ def build_zeynab_family():
     search_for_person(sister, "Jedd")
 
 
-# Function to create a person project to be added to the family tree
 def create_person():
+    """Function to create a person project to be added to the family tree"""
     name = input("Name: ")
     birthdate = input("Date of Birth (DD-MM-YYYY): ")
     gender = input("Gender(F/M): ")
@@ -87,13 +89,15 @@ def create_person():
     new_person = Person(name, birthdate, gender, occupation)
     return new_person
 
-# Function to add a new person object to the family tree
+
 def add_person(person, family_dict):
+    """Function to add a new person object to the family tree"""
     if person.name not in family_dict:
         family_dict[person.name] = person
 
-# Function to delete a person and all of its relations with other people from family tree
+
 def delete_person(person, family_dict):
+    """Function to delete a person and all of its relations with other people from family tree"""
     if person.name in family_dict:
         family_dict.pop(person.name)
         for member in family_dict.values():
@@ -105,14 +109,19 @@ def delete_person(person, family_dict):
                         del member.relation_dict[relation]
     return family_dict
 
-# Function to print the current family tree
+
 def print_family_dict(family_dict):
+    """Function to print the current family tree"""
     print("The Family:\n")
     for key, value in family_dict.items():
         print(f'{key},{value}')
 
+
 def search_for_person(first_person, second_person_name):
-    """BFS searching the relations between two people in a family tree, take 1 Person object and 1 name as parameter"""
+    """
+    BFS searching the relations between two people in a family tree
+    take 1 Person object and 1 name as parameter
+    """
     relationship_stack = deque()
     relationship_stack.append([first_person])
 
@@ -134,21 +143,24 @@ def search_for_person(first_person, second_person_name):
 
     print(f"{second_person_name} cannot be founded to connect to {first_person.name}")
 
-# Function to find the relation between two immediate family members
+
 def find_immediate_relation(first_person, second_person):
+    """Function to find the relation between two immediate family members"""
     for key, value in first_person.relation_dict.items():
         if second_person in value:
             return key
     return None
 
-# Function to print the path from one person to another person (show their relations and the relatives involved)
+
 def print_list_of_person(people_list):
+    """Function to print the path from one person to another person (show their relations and the relatives involved)"""
     out_put = ""
     for i in range(len(people_list)-1):
         out_put += people_list[i].name + f"('s {find_immediate_relation(people_list[i],people_list[i+1])})" + "->"
     out_put += people_list[-1].name
     print(out_put)
     print(f"The relationship distance from {people_list[0].name} to {people_list[-1].name} is {len(people_list)-1} step(s).")
+
 
 def save_family_to_json(family_dict, filename="Zeynab_family_tree.json"):
     """Save the family data to a JSON file."""
@@ -159,6 +171,7 @@ def save_family_to_json(family_dict, filename="Zeynab_family_tree.json"):
     with open(filename, "w") as file:
         json.dump(json_data, file, indent=4)
     print(f"Family tree saved to {filename}")
+
 
 def load_family_from_json(filename="Zeynab_family_tree.json"):
     """Load the family tree data from a JSON file and reconstruct the family graph."""
@@ -186,4 +199,3 @@ def load_family_from_json(filename="Zeynab_family_tree.json"):
 
     print(f"Family tree loaded from {filename}")
     return family_dict
-
